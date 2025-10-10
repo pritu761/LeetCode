37a10e15dc2ed9d3fdfe041ba2dd04ebda1e118d
Pritam Kumar Nath <pknath769@gmail.com>
2025-10-11 03:39:53 +0530

    Time: 150 ms (24.09%) | Memory: 151.8 MB (22.77%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    int n;
-    int K;
-
-    int solve(int i, vector<int>& energy, vector<int>& t) {
-        if (i >= n) 
-            return 0;
-        
-        if (t[i] != INT_MIN)
-            return t[i];
-
-        // Take energy at i and then best from (i + k) onwards - Recursion leap of faith
-        t[i] = energy[i] + solve(i + K, energy, t);
-        
-        return t[i];
-    }
-
     int maximumEnergy(vector<int>& energy, int k) {
-        n = energy.size();
-        K = k;
-        vector<int> t(n, INT_MIN);     // Initialize memoization array
+        int n = energy.size();
+        vector<int> t(n, 0); // DP array
 
-        int maxEnergy = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            maxEnergy = max(maxEnergy, solve(i, energy, t));
+        for (int i = n - 1; i >= 0; i--) { // see my video to understand why right to left traversal is needed here
+            if (i + k < n)
+                t[i] = energy[i] + t[i + k];
+            else
+                t[i] = energy[i];
         }
 
-        return maxEnergy;
+        return *max_element(begin(t), end(t));
     }
 };
