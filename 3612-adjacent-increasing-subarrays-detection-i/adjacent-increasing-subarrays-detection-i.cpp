27543c984a3d5b334e6f7138ec24a53e1bd3d22f
Pritam Kumar Nath <pknath769@gmail.com>
2025-10-18 00:32:55 +0530

    Time: 12 ms (65.16%) | Memory: 40.5 MB (46.37%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -1,23 +1,52 @@
+// class Solution {
+// public:
+//     bool isIncreasing(vector<int>& nums, int start, int end) {
+//         for (int i = start + 1; i < end; i++) {
+//             if (nums[i] <= nums[i - 1])
+//                 return false;
+//         }
+//         return true;
+//     }
+
+//     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         for (int start = 0; start + 2 * k <= n; start++) {
+//             bool firstInc = isIncreasing(nums, start, start + k);
+//             bool secondInc = isIncreasing(nums, start + k, start + 2 * k);
+
+//             if (firstInc && secondInc)
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
-    bool isIncreasing(vector<int>& nums, int start, int end) {
-        for (int i = start + 1; i < end; i++) {
-            if (nums[i] <= nums[i - 1])
-                return false;
-        }
-        return true;
-    }
-
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
 
-        for (int start = 0; start + 2 * k <= n; start++) {
-            bool firstInc = isIncreasing(nums, start, start + k);
-            bool secondInc = isIncreasing(nums, start + k, start + 2 * k);
+        int currRun = 1;
+        int prevRun = 0;
 
-            if (firstInc && secondInc)
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i-1]) {
+                currRun++;
+            } else { //increaseing order break hogaya yaha par
+                prevRun = currRun;
+                currRun = 1;
+            }
+
+            if(currRun >= 2*k) {
+                //we can accomodate two subarrays in it length. >= k
                 return true;
+            }
+
+            if(min(currRun, prevRun) >= k) {
+                return true;
+            }
         }
+
         return false;
     }
 };
