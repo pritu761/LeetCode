d1ddb449866a195241a2435d324cd8517224c312
Pritam Kumar Nath <pknath769@gmail.com>
2025-10-24 03:46:25 +0530

    Time: 39 ms (94.11%) | Memory: 83.4 MB (80.73%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -1,39 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        sort(nums.begin(),
+             nums.end()); // Sort the array for sliding window approach
+
         int n = nums.size();
-        if (n == 0) return 0;
-        sort(nums.begin(), nums.end());
+        int left1 = 0,
+            right1 = 0;       // Window pointers for first frequency calculation
+        int windowCount1 = 0; // Count of elements within [num - k, num + k]
+        int maxFreq = 0;      // Final answer (maximum frequency)
+        int left2 = 0;        // Second window left pointer for extended range
+        int windowCount2 = 0; // Count of elements within [num - 2k, num]
+        int sameValueCount = 0; // Frequency of current number
+        int prevValue = -1;     // Previous number for counting duplicates
 
-        unordered_map<long long,int> freq;
-        freq.reserve(n*2);
-        for (int x: nums) freq[x]++;
+        for (auto num : nums) {
+            // Update count of consecutive same numbers
+            if (num == prevValue)
+                sameValueCount++;
+            else {
+                prevValue = num;
+                sameValueCount = 1;
+            }
 
-        int ans = 1;
+            // Shrink window1 if left element is out of range [num - k, num + k]
+            while (nums[left1] < num - k) {
+                windowCount1--;
+                left1++;
+            }
 
-        for (auto &p : freq) {
-            long long v = p.first;
-            int already = p.second;
-            long long lowVal = v - k;
-            long long highVal = v + k;
-            auto L = lower_bound(nums.begin(), nums.end(), (int)lowVal);
-            auto R = upper_bound(nums.begin(), nums.end(), (int)highVal);
-            int totalInRange = int(R - L);
-            int need = totalInRange - already;
-            int canFix = min(need, numOperations);
-            ans = max(ans, already + canFix);
+            // Expand window1 to include all elements <= num + k
+            while (right1 < n && nums[right1] <= num + k) {
+                windowCount1++;
+                right1++;
+            }
+
+            // Maximize frequency by replacing up to numOperations elements
+            maxFreq =
+                max(maxFreq, sameValueCount + min(windowCount1 - sameValueCount,
+                                                  numOperations));
+
+            // Update secondary window for range [num - 2k, num]
+            windowCount2++;
+            while (nums[left2] < num - 2 * k) {
+                windowCount2--;
+                left2++;
+            }
+
+            // Consider extending range fully within 2*k window
+            maxFreq = max(maxFreq, min(windowCount2, numOperations));
         }
 
-        int l = 0;
-        for (int r = 0; r < n; ++r) {
-            while (l <= r && (long long)nums[r] - nums[l] > 2LL * k) ++l;
-            int w = r - l + 1;
-            ans = max(ans, min(w, numOperations));
-        }
-
-        return ans;
+        return maxFreq;
     }
 };
