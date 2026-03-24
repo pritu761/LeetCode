f459bffe8fc61e6a04b62f62c2b2e351acba37ea
Pritam Kumar Nath <pritamkumarnath01@gmail.com>
2026-03-25 02:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (76.76%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int count, int i, int direction) {
-        vector<int> temp = nums;
-        int idx = i;
-
-        while (count > 0 && idx >= 0 && idx < nums.size()) {
-            if (temp[idx] > 0) {
-                temp[idx]--;
-                direction *= -1;
-                if (temp[idx] == 0) {
-                    count--;
-                }
-            }
-            idx += direction;
-        }
-
-        return count == 0; //all must become 0
-    }
-
     int countValidSelections(vector<int>& nums) {
-        int result = 0;
-        int count = 0; //count of nums[i] != 0
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                count++;
-            }
-        }
+        int result =0;
+        int curr = 0;
+        int S = accumulate(begin(nums), end(nums), 0);//for loop bhi likh sakte ho
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                if (isValid(nums, count, i, -1)) {
-                    result++;
-                }
-                if (isValid(nums, count, i, 1)) {
-                    result++;
-                }
+
+        for(int i = 0; i < n; i++) {
+            curr += nums[i]; //left sum
+
+            int left  = curr;
+            int right = S - left;
+
+            if(nums[i] != 0) {
+                continue;
             }
+
+            if(left == right)
+                result += 2;
+            
+            if(abs(left - right) == 1)
+                result += 1;
         }
 
         return result;
